d583ba73e1d76fd7d201637ce23a7fdd57ab2cce
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2022-10-22 12:06:46 +0200

    feat(client): installed solid-styled with open-props

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import solidStyled from 'vite-plugin-solid-styled';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    solidStyled({
+      filter: {
+        include: 'src/**/*.{ts,tsx}',
+        exclude: 'node_modules/**/*.{ts,tsx,js,jsx}',
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
